20494d68975bd4eb902e3cb71952f02e7ea57431
changha <mentenseoul@gmail.com>
2024-11-21 20:50:56 +0900

    Chore : api 통일
    /api/v1 빠진 부분 채웠습니다.

--- src/main/java/com/example/back/teamate/controller/UsersController.java
+++ src/main/java/com/example/back/teamate/controller/UsersController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.back.teamate.dto.RedisUserInfoDto;
@@ -15,6 +16,7 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class UsersController {
 
 	private final UsersService usersService;
