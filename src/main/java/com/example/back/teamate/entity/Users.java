f3943fdea51bf3b5e747cc1a80ee6d9f951149e3
changha <mentenseoul@gmail.com>
2024-11-22 13:03:49 +0900

    Refactor : Users 전화번호 필드 삭제
    Users의 전화번호 필드를 삭제하였습니다 (이메일로 알림받으니까)

--- src/main/java/com/example/back/teamate/entity/Users.java
+++ src/main/java/com/example/back/teamate/entity/Users.java
@@ -22,8 +22,8 @@ public class Users extends BaseEntity {
 	@Column(nullable = false)
 	private String email; // 이메일
 
-	private String phoneNumber; // 전화번호 (선택 동의)
 	private String nickname; // 닉네임
+
 	private String profileImage; // 프로필 이미지
 
 	private int age; // 나이
@@ -51,11 +51,6 @@ public class Users extends BaseEntity {
 
 	private boolean isActiveProject; // 프로젝트 활성화 여부
 	private boolean isActiveStudy; // 스터디 활성화 여부
-
-	// 추가 메서드 (전화번호, 이메일 변경 등)
-	public void changePhoneNumber(String newPhoneNumber) {
-		this.phoneNumber = newPhoneNumber;
-	}
 
 	public void changeEmail(String newEmail) {
 		this.email = newEmail;
