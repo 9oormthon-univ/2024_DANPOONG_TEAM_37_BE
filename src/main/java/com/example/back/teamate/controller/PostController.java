921650ec2bbe59ad3e50a7815ca01239b3791643
changha <mentenseoul@gmail.com>
2024-11-23 10:33:35 +0900

    Feat : 게시글 리스트 조회 API 작성
    게시글 리스트 작성에 필요한 로직을 추가하였습니다.

--- src/main/java/com/example/back/teamate/controller/PostController.java
+++ src/main/java/com/example/back/teamate/controller/PostController.java
@@ -1,29 +1,46 @@
 package com.example.back.teamate.controller;
 
+import com.example.back.teamate.dto.ApiResponse;
+import com.example.back.teamate.dto.PostListResponseDto;
 import com.example.back.teamate.dto.PostRequestDto;
+import com.example.back.teamate.dto.RedisUserInfoDto;
 import com.example.back.teamate.service.PostService;
+import com.example.back.teamate.service.TokenAuthenticationService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import java.util.List;
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/posts")
+@RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
-
-    public PostController(PostService postService) {
-        this.postService = postService;
-    }
+    private final TokenAuthenticationService tokenAuthenticationService;
 
     @PostMapping
-    public ResponseEntity<?> createPost(@RequestBody @Valid PostRequestDto postRequestDto) {
-        Long postId = postService.createPost(postRequestDto);
+    public ResponseEntity<?> createPost(@RequestHeader("Authorization") String authHeader, @RequestBody @Valid PostRequestDto postRequestDto) {
+        RedisUserInfoDto redisUserInfoDto = tokenAuthenticationService.authenticateUser(authHeader);
+
+        Long postId = postService.createPost(redisUserInfoDto.getId(), postRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED)
-            .body(Map.of("message", "Post created successfully."));
+            .body(Map.of("message", "Post created successfully. postId"));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<PostListResponseDto>>> getPostList(
+        @RequestParam(value = "page", defaultValue = "0") int page,
+        @RequestParam(value = "size", defaultValue = "10") int size) {
+        List<PostListResponseDto> posts = postService.getPostListAsList(page, size);
+        return ResponseEntity.ok(ApiResponse.createSuccess(posts));
     }
 }
--- src/main/java/com/example/back/teamate/service/PostService.java
+++ src/main/java/com/example/back/teamate/service/PostService.java
@@ -1,27 +1,45 @@
 package com.example.back.teamate.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.example.back.teamate.dto.PostListResponseDto;
 import com.example.back.teamate.dto.PostRequestDto;
+import com.example.back.teamate.entity.Field;
 import com.example.back.teamate.entity.Position;
 import com.example.back.teamate.entity.Post;
 import com.example.back.teamate.entity.PostPosition;
 import com.example.back.teamate.entity.PostSkill;
+import com.example.back.teamate.entity.Role;
 import com.example.back.teamate.entity.Skill;
+import com.example.back.teamate.entity.Users;
 import com.example.back.teamate.enums.FieldName;
 import com.example.back.teamate.enums.ModeName;
 import com.example.back.teamate.enums.PositionName;
 import com.example.back.teamate.enums.SkillName;
+import com.example.back.teamate.enums.TeamRole;
 import com.example.back.teamate.repository.FieldRepository;
 import com.example.back.teamate.repository.ModeRepository;
 import com.example.back.teamate.repository.PositionRepository;
 import com.example.back.teamate.repository.PostPositionRepository;
 import com.example.back.teamate.repository.PostRepository;
 import com.example.back.teamate.repository.PostSkillRepository;
+import com.example.back.teamate.repository.RoleRepository;
 import com.example.back.teamate.repository.SkillRepository;
+import com.example.back.teamate.repository.UsersRepository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class PostService {
     private final PostRepository postRepository;
     private final FieldRepository fieldRepository;
@@ -30,20 +48,21 @@ public class PostService {
     private final SkillRepository skillRepository;
     private final PostPositionRepository postPositionRepository;
     private final PostSkillRepository postSkillRepository;
+    private final RoleRepository roleRepository;
+    private final UsersRepository usersRepository;
 
-    public PostService(PostRepository postRepository, FieldRepository fieldRepository, ModeRepository modeRepository,
-                        PositionRepository positionRepository, SkillRepository skillRepository,
-                        PostPositionRepository postPositionRepository, PostSkillRepository postSkillRepository) {
-        this.postRepository = postRepository;
-        this.fieldRepository = fieldRepository;
-        this.modeRepository = modeRepository;
-        this.positionRepository = positionRepository;
-        this.skillRepository = skillRepository;
-        this.postPositionRepository = postPositionRepository;
-        this.postSkillRepository = postSkillRepository;
-    }
+    public Long createPost(Long userId, PostRequestDto postRequestDto) {
+        // 사용자 조회
+        Users user = usersRepository.findById(userId)
+            .orElseThrow(() -> new IllegalArgumentException("Invalid userId: " + userId));
 
-    public Long createPost(PostRequestDto postRequestDto) {
+        // 역할 생성 및 저장
+        Role role = Role.builder()
+            .teamRole(TeamRole.TEAM_LEADER)
+            .user(user)
+            .build();
+        roleRepository.save(role);
+
         // field값을 프로젝트,스터디 중 뭘로 받았는지 확인하고 enum값으로 변환
         FieldName fieldName = FieldName.fromDisplayName(postRequestDto.getField());
         ModeName modeName = ModeName.fromDisplayName(postRequestDto.getMode());
@@ -57,6 +76,7 @@ public class PostService {
         post.setDeadline(postRequestDto.getDeadline());
         post.setFieldId(fieldName.getFieldId());
         post.setModeId(modeName.getModeId());
+        post.setRole(role);
         Post savedPost = postRepository.save(post);
 
         //URL 값 저장
@@ -94,6 +114,47 @@ public class PostService {
             }
         }
         return savedPost.getPostId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostListResponseDto> getPostListAsList(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
+        Page<Post> posts = postRepository.findAll(pageable);
+
+        // Page<Post>를 List<PostListResponseDto>로 변환
+        return posts.stream().map(post -> {
+            // 1. Position 데이터 조회
+            List<PositionName> positions = postPositionRepository.findByPost_PostId(post.getPostId()).stream()
+                .map(postPosition -> postPosition.getPosition().getPositionName())
+                .collect(Collectors.toList());
+
+            // 2. Skill 데이터 조회
+            List<SkillName> skills = postSkillRepository.findByPost_PostId(post.getPostId()).stream()
+                .map(postSkill -> postSkill.getSkill().getSkillName())
+                .collect(Collectors.toList());
+
+            // 3. Field 데이터 조회
+            Field field = fieldRepository.findByFieldId(post.getFieldId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid field id: " + post.getFieldId()));
+
+            // 4. Author 데이터 조회
+            Role author = roleRepository.findByPosts_PostId(post.getPostId())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid role id: " + post.getPostId()));
+
+            // 5. DTO로 변환
+            return PostListResponseDto.builder()
+                .postId(post.getPostId())
+                .title(post.getTitle())
+                .field(field.getFieldName()) // FieldName을 변환
+                .position(positions) // 리스트로 변환한 포지션
+                .skill(skills)       // 리스트로 변환한 스킬
+                .deadline(post.getDeadline().toString())
+                .author(PostListResponseDto.AuthorInfo.builder()
+                    .nickname(author.getUser().getNickname())
+                    .profileImage(author.getUser().getProfileImage())
+                    .build())
+                .build();
+        }).collect(Collectors.toList());
     }
 
 }
