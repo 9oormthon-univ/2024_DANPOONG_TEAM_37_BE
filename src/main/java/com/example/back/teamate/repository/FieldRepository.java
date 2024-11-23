90130952c8720de12a0070050b2ff58a3c99db58
changha <mentenseoul@gmail.com>
2024-11-23 10:32:12 +0900

    Refactor : Repository 메서드 추가
    필요한 메서드를 추가하였습니다.

--- src/main/java/com/example/back/teamate/repository/FieldRepository.java
+++ src/main/java/com/example/back/teamate/repository/FieldRepository.java
@@ -1,9 +1,12 @@
 package com.example.back.teamate.repository;
 
+import java.util.Optional;
+
 import com.example.back.teamate.entity.Field;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FieldRepository extends JpaRepository<Field, Long> {
+	Optional<Field> findByFieldId(int fieldId);
 }
--- src/main/java/com/example/back/teamate/repository/PostSkillRepository.java
+++ src/main/java/com/example/back/teamate/repository/PostSkillRepository.java
@@ -1,9 +1,12 @@
 package com.example.back.teamate.repository;
 
+import java.util.List;
+
 import com.example.back.teamate.entity.PostSkill;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostSkillRepository extends JpaRepository<PostSkill, Long> {
+	List<PostSkill> findByPost_PostId(Long postId); // 특정 게시글에 해당하는 스킬 목록 조회
 }
